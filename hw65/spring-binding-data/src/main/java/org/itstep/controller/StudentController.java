c8695a3f84891f9a194427ad1f0391c044e1bd56
Vlad <vladooss22@gmail.com>
2020-02-17 15:32:20 +0200

    add hw 65 (spring students)

---
+++ hw65/spring-binding-data/src/main/java/org/itstep/controller/StudentController.java
@@ -0,0 +1,73 @@
+package org.itstep.controller;
+
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import org.itstep.data.StudentRepository;
+import org.itstep.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/students")
+@Controller
+public class StudentController {
+
+    final StudentRepository repository;
+
+    @Autowired
+    public StudentController(StudentRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("students", repository.findAll());
+        return "students/index";
+    }
+
+    @GetMapping("/new")
+    public String createStudent() {
+        return "students/create";
+    }
+
+    @PostMapping("/new")
+    public String createStudent(Student student) {
+        int id = repository.save(student);
+        return "redirect:/students/info/" + id;
+    }
+
+    @GetMapping("/info/{id}")
+    public String info(@PathVariable int id, Model model) {
+        model.addAttribute("student", repository.find(id));
+        return "students/info";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(Model model, @PathVariable int id) {
+        Student student = repository.find(id);
+        if (student != null) {
+            repository.delete(student);
+        }
+        return "redirect:/students/index";
+    }
+
+    @GetMapping("/update/{id}")
+    public String update(Model model, @PathVariable int id) {
+        Student student = repository.find(id);
+        if (student != null) {
+            model.addAttribute("student", student);
+            return "/students/update";
+        }
+        return "redirect:/students/index";
+    }
+
+    @PostMapping("/update/{id}")
+    public String update(Student student){
+        repository.update(student);
+        return "redirect:/students/info/" + student.getId();
+    }
+
+}
---
+++ hw65/spring-binding-data/src/main/webapp/WEB-INF/view/students/update.jsp
@@ -0,0 +1,37 @@
+<%--@elvariable id="student" type="org.itstep.model.Student"--%>
+<%@ taglib prefix="c" uri="http://www.springframework.org/tags" %>
+
+<%@ page contentType="text/html;charset=UTF-8" language="java"  isELIgnored="false" %>
+<html>
+<head>
+    <title>Create new student</title>
+    <link href="<c:url value="/static/css/style.css"/>" rel="stylesheet"/>
+</head>
+<body>
+    <h1>Create new student</h1>
+<form method="post">
+    <input type="text" value="${student.id}" name="id" hidden>
+    <div>
+        <label for="firstName">First name: </label>
+        <input name="firstName" value="${student.firstName}" id="firstName" required/>
+    </div>
+    <div>
+        <label for="lastName">Last name: </label>
+        <input name="lastName" value="${student.lastName}" id="lastName" required/>
+    </div>
+    <div>
+        <label for="age">Age: </label>
+        <input name="age" id="age" value="${student.age}" type="number" min="8" max="60" required/>
+    </div>
+
+    <div>
+        <label for="group">Group: </label>
+        <input name="group" value="${student.group}" id="group" required/>
+    </div>
+
+    <div>
+        <input type="submit"/>
+    </div>
+</form>
+</body>
+</html>
